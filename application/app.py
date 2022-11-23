37f9257bf8d3b90faf78f36fc8d93bc5566372a8
AUGUSTE Pierre <pierre.auguste@telecomnancy.eu>
2022-11-23 09:51:36 +0100

    requirement

---
+++ application/app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+
+app=Flask(__name__)
+
+@app.route('/')
+def homeDev():
+    return render_template('homeDev.html')
+
+@app.route('/testLayout')
+def testLayout():
+    return render_template("testLayout.html")
