40e61d07e631e1745bb2b59ac977c1bd1491d950
PierRapt <pierre.aug@orange.fr>
2022-11-30 23:39:43 +0100

    Layout Unconnected

--- application/app.py
+++ application/app.py
@@ -9,7 +9,7 @@ app=Flask(__name__)
 
 @app.route('/')
 def homeDev():
-    return render_template('homeDev.html')
+    return render_template('homeUnconnected.html')
 
 @app.route('/testLayout')
 def testLayout():
