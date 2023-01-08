02e63dca4f529f5910a891f88e41b0001370edb5
Manon Lécubin <Manon.Lecubin@telecomnancy.eu>
2023-01-08 11:06:29 +0100

    mdp changés

--- application/app.py
+++ application/app.py
@@ -407,19 +407,19 @@ def crypte_mdp(mdp):
     mdplist=[]
     for x in mdp:
         mdplist.append(x)
-    def aux(mdplist,ind_ascii,ind_list):
+    def aux(mdplist,ind_list):
         if len(mdplist)==0:
             for i in range(len(mdp_crypte)):
                 while mdp_crypte[i]>=len(ascii):
                     mdp_crypte[i]=mdp_crypte[i]-len(ascii) 
             return ''.join(ascii[n] for n in mdp_crypte)
-        if mdplist[0]!=ascii[ind_ascii]:
+        ind_ascii=0
+        while mdplist[0]!=ascii[ind_ascii]:
             for i in range(ind_list,len(mdp_crypte)):
                 mdp_crypte[i]+=1
-            return aux(mdplist,ind_ascii+1,ind_list)
-        return aux(mdplist[1:],0,ind_list+1)
-    return aux (mdplist,0,0)
-
+            ind_ascii+=1
+        return aux(mdplist[1:],ind_list+1)
+    return aux (mdplist,0)
 
 REGIONS = {
     'Auvergne-Rhône-Alpes': ['01', '03', '07', '15', '26', '38', '42', '43', '63', '69', '73', '74'],
